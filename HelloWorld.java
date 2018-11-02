6836f8e3e27507e1bbf490ac6bd5284cc98d6705
Jyotisingh11 <44695351+Jyotisingh11@users.noreply.github.com>
2018-11-02 01:07:59 -0700

    World class added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,3 +4,10 @@ public static void main(String args[])
 {
 System.out.println("hello World");
 }}
+public class World
+{
+  public static void main(String a[])
+  {
+    System.out.println("hey");
+  }}
+                       
